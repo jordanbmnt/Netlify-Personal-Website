d3bf147514f48ec79fd6ab1317495e1e3a80f0f9
Jordan Maurice Beaumont <jordanbmnt@gmail.com>
2023-09-12 19:04:02 +0200

    added basic info pos

--- src/Pages/MainPage/MainPage.jsx
+++ src/Pages/MainPage/MainPage.jsx
@@ -5,9 +5,10 @@ import { Button } from "@mui/material"
 export const MainPage = () => {
   return (
     <Container sx={{display: 'flex'}} className='main-page'>
-      <h1>Hello</h1>
+      <h1>I am Jordan M Beaumont</h1>
+      <h1 id="key-word">Web developer.</h1>
       <h4>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed, magnam quidem nostrum praesentium quisquam suscipit, qui optio hic distinctio, voluptatibus aliquid. Nam, culpa. Aliquam libero sed quod maxime laboriosam officia debitis, laudantium eos ullam alias fugit ipsum deserunt incidunt dicta nihil expedita tenetur! Voluptates delectus aspernatur autem eos perferendis quis.</h4>
-      <Button variant="outlined">Generate</Button>
+      <Button id="generate-button" variant="outlined">Generate</Button>
     </Container>
   )
 }
