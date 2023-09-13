482a4d8289683b5a770a1fb433f8567bb5cf9f2e
Jordan Maurice Beaumont <jordanbmnt@gmail.com>
2023-09-13 16:33:38 +0200

    added style

--- src/Pages/MainPage/MainPage.jsx
+++ src/Pages/MainPage/MainPage.jsx
@@ -1,14 +1,20 @@
 import { Container } from "@mui/system"
 import './mainPage.css'
-import { Button } from "@mui/material"
+import { Button } from "@mui/material";
+import earth from "../../Images/planetEarth.png"
+import spaceMan from "../../Images/astronautThreeQuarterBackView.png"
 
 export const MainPage = () => {
   return (
     <Container sx={{display: 'flex'}} className='main-page'>
+      <Container className="heading-container">
       <h1>I am Jordan M Beaumont</h1>
       <h1 id="key-word">Web developer.</h1>
       <h4>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed, magnam quidem nostrum praesentium quisquam suscipit, qui optio hic distinctio, voluptatibus aliquid. Nam, culpa. Aliquam libero sed quod maxime laboriosam officia debitis, laudantium eos ullam alias fugit ipsum deserunt incidunt dicta nihil expedita tenetur! Voluptates delectus aspernatur autem eos perferendis quis.</h4>
+      </Container>
       <Button id="generate-button" variant="outlined">Generate</Button>
+      <img id="main-earth" src={earth} alt="" />
+      <img id="main-spaceman" src={spaceMan} alt="" />
     </Container>
   )
 }
