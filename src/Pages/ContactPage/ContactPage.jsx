a531de6deaf0761a31675892384996a2a6cf57c6
Jordan Maurice Beaumont <jordanbmnt@gmail.com>
2023-12-27 23:31:02 +0200

    added new clouds obj

--- src/Pages/ContactPage/ContactPage.jsx
+++ src/Pages/ContactPage/ContactPage.jsx
@@ -1,16 +1,6 @@
 import { Container } from "@mui/system";
 import "./contactPage.css";
-import Spline from "@splinetool/react-spline";
 
 export const ContactPage = () => {
-  return (
-    <Container sx={{ display: "flex" }} className='hero-page'>
-      <Container>
-        <h1>
-          hello
-          <Spline scene='https://prod.spline.design/BVCqfL6og-M0RKFp/scene.splinecode' />
-        </h1>
-      </Container>
-    </Container>
-  );
+  return <Container sx={{ display: "flex" }} className='hero-page'></Container>;
 };
--- src/Pages/HeroPage/HeroPage.jsx
+++ src/Pages/HeroPage/HeroPage.jsx
@@ -1,19 +1,44 @@
 import { Container } from "@mui/system";
 import "./heroPage.css";
-import cloud from "../../Images/whiteFluffyCloud.png";
+import Spline from "@splinetool/react-spline";
+import { useRef, useState } from "react";
 
 export const HeroPage = () => {
+  const cloudOne = useRef();
+  const cloudTwo = useRef();
+  const cloudThree = useRef();
+  const [pos, setPos] = useState(0);
+  function onLoad(spline) {
+    const obj = spline.findObjectById("40cab445-4094-4332-a8d9-9d0a8d3025b7");
+    const obj2 = spline.findObjectById("eb31b5fc-57ec-45cf-b47f-9ebba36a44c6");
+    const obj3 = spline.findObjectById("97731825-e852-4f04-8e7c-d31ae12482aa");
+    cloudOne.current = obj;
+    cloudTwo.current = obj2;
+    cloudThree.current = obj3;
+
+    if (cloudOne.current) {
+      window.addEventListener("scroll", function () {
+        setPos(Math.floor(this.scrollY - window.innerHeight));
+        cloudOne.current.position.x += 100;
+      });
+      // cloudOne.current.position.x += pos * 100;
+      // cloudTwo.current.position.x += pos * 100 * 3;
+      // cloudThree.current.position.x += pos * 100 * 4;
+    }
+  }
   return (
     <Container
       sx={{ display: "flex", scrollSnapAlign: "start" }}
       className='hero-page'
     >
-      <Container>
-        <h1>
-          hello
-          <img id='hero-cloud' src={cloud} alt='' />
-        </h1>
+      <Container id='content-box'>
+        <h1>{pos}</h1>
       </Container>
+      <Spline
+        onLoad={onLoad}
+        id='spline-clouds'
+        scene='https://draft.spline.design/WpGfUOX7-iXG2VgG/scene.splinecode'
+      />
     </Container>
   );
 };
