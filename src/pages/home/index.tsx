15ac90e812542602a419de8904fc57335505fb12
jordanbmnt <jordanbmnt@gmail.com>
2025-10-23 18:47:13 +0200

    added minimal contect to fix responsiveness

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,13 @@
 export const Home = () => {
   return (
     <div>
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the main landing page of the website.</p>
+      <div className="intro">
+        <div>
+          <h1>Welcome to the Home Page</h1>
+          <p>This is the main landing page of the website.</p>
+        </div>
+        <img src="https://cdn.pixabay.com/photo/2025/05/23/06/35/sparrow-9617024_1280.jpg" alt="" />
+      </div>
     </div>
   );
 }
