1256f6a9119442f5e7029f9ee55eacba20d67eb5
jordanbmnt <jordanbmnt@gmail.com>
2025-10-31 22:34:31 +0200

    improved semantics and started using @starting-style

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,13 @@
 export const Home = () => {
   return (
     <div>
-      <div className="intro">
+      <section className="intro">
         <div>
           <h1>Welcome to the Home Page</h1>
           <p>This is the main landing page of the website.</p>
         </div>
         <img src="https://cdn.pixabay.com/photo/2025/05/23/06/35/sparrow-9617024_1280.jpg" alt="" />
-      </div>
+      </section>
     </div>
   );
 }
